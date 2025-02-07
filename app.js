450066b6dd2e480663a54241defae8930e06d75a
Chetan Kala <chetankala1487@gmail.com>
2025-02-07 23:33:25 +0530

    minor bug4

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const cookiParser = require("cookie-parser")
 // setup
 connect()
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://auth-frontend-hazel.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+    req.header("Access-Control-Allow-Origin", "https://auth-frontend-hazel.vercel.app");
+    req.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    req.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    req.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
